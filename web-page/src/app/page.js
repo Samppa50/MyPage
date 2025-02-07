0a8e2e4421cacc2da5b6d23d63dc6a1cf8c0995e
Samppa50 <silvansami50@gmail.com>
2025-02-07 16:58:10 +0200

    added bingo

--- web-page/src/app/page.js
+++ web-page/src/app/page.js
@@ -1,11 +1,10 @@
-import Helloworld from "./components/textbox";
+import Bingo from "./components/bingo";
 import Tictactoe from "./components/tictactoe";
 
 export default function Home() {
   return (
     <div>
-      <Helloworld></Helloworld>
-      <Tictactoe></Tictactoe>
+      <Bingo></Bingo>
     </div>
   );
 }
