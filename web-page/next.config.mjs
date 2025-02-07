5463bd3af2edbbd101a324e36dda19f7ae5f1415
Samppa50 <silvansami50@gmail.com>
2025-02-07 18:25:22 +0200

    css hopefully working

--- web-page/next.config.mjs
+++ web-page/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: {
-        unoptimized: true,
-    },
-};
-
-export default nextConfig;
+    basePath: '/MyPage',
+    assetPrefix: '/MyPage/',
+    reactStrictMode: true,
+    swcMinify: true,
+  };
+  
+  module.exports = nextConfig;
